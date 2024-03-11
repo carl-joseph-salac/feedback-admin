6007da1b07f680e55be2d31741ecfea19ed33189
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-03-11 21:30:26 +0800

    first commit

---
+++ resources/views/layout/script.blade.php
@@ -0,0 +1,6 @@
+<!-- jQuery -->
+<script src="{{ asset('AdminLTE-3.2.0/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('AdminLTE-3.2.0/dist/js/adminlte.min.js') }}"></script>
